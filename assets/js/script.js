dcb1cb19983daf85d9a74cf41b1d58b0cd673c6b
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2024-08-10 19:52:45 +0700

    up register

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,18 +9,18 @@ $(document).ready(function () {
         center: true,
         navText: [
             "<i class='fa fa-angle-left'></i>",
-            "<i class='fa fa-angle-right'></i>"
+            "<i class='fa fa-angle-right'></i>",
         ],
         responsive: {
             0: {
-                items: 3
+                items: 3,
             },
             600: {
-                items: 3
+                items: 3,
             },
             1000: {
-                items: 3
-            }
+                items: 3,
+            },
         },
         autoplaySpeed: 4000,
     });
@@ -35,49 +35,45 @@ $(document).ready(function () {
         center: true,
         navText: [
             "<i class='fa fa-angle-left'></i>",
-            "<i class='fa fa-angle-right'></i>"
+            "<i class='fa fa-angle-right'></i>",
         ],
         responsive: {
             0: {
-                items: 1
+                items: 1,
             },
             600: {
-                items: 1
+                items: 1,
             },
             1000: {
-                items: 3
-            }
+                items: 3,
+            },
         },
         autoplaySpeed: 4000,
     });
 });
 
-function changeColor() {
-    var icon = document.getElementById("myIcon");
-    var iconPath = document.getElementById("iconPath");
-    if (!icon.classList.contains("clicked")) {
-        icon.classList.add("clicked");
-        iconPath.setAttribute("fill", "#4AB273");
+// Back to top button
+var backToTopBtn = document.getElementById("backToTopBtn");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 100 ||
+        document.documentElement.scrollTop > 100
+    ) {
+        backToTopBtn.style.display = "block";
     } else {
-        icon.classList.remove("clicked");
-        iconPath.setAttribute("fill", "rgba(0, 0, 0, 0.20);");
+        backToTopBtn.style.display = "none";
     }
 }
 
-const progressBar = document.getElementById('progress');
-progressBar.style.width = '100%';
-
-function resetProgressBar() {
-    const progressBar = document.getElementById('progress');
-
-    // Set lebar menjadi 0 tanpa transisi
-    progressBar.style.transition = 'width 0s';
-    progressBar.style.width = '0px';
-
-    // Menerapkan transisi kembali ke 100% pada frame selanjutnya
-    requestAnimationFrame(() => {
-        progressBar.style.transition = 'width 30s linear';
-        progressBar.style.width = '100%';
+backToTopBtn.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
     });
-}
-
+};
